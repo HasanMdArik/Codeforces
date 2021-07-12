157a9f54dc6137cf56f35290ca7dcccb553c6822
MohammadArik <mohammadarik63@gmail.com>
2021-07-12 21:42:40 +0600

    282A solution

---
+++ Difficulty 800/282A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    x = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        string statement;
+        cin >> statement;
+
+        if (statement == "++X" || statement == "X++" || statement == "++")
+        {
+            x++;
+        }
+        else if (statement == "--X" || statement == "X--" || statement == "--")
+        {
+            x--;
+        }
+    }
+
+    cout << x;
+
+    return 0;
+}
