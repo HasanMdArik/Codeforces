ee117e884be81896af3ee4f3d7b96512667bee9f
Mohammad Arik <mohammadarik63@gmail.com>
2021-07-13 16:14:15 +0600

    263A Solved

---
+++ Difficulty 800/263A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// https://codeforces.com/problemset/problem/263/A
+
+int main(int argc, char const *argv[])
+{
+    int columnOf1, rowOf1;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            int cell;
+            cin >> cell;
+            if (cell == 1)
+            {
+                columnOf1 = j;
+                rowOf1 = i;
+                break;
+            }
+        }
+    }
+
+    int horizontalMoves = abs(rowOf1 - 2);
+    int verticalMoves = abs(columnOf1 - 2);
+
+    cout << (horizontalMoves + verticalMoves) << endl;
+
+    return 0;
+}
