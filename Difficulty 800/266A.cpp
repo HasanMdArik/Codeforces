e684263f009e813095a17ff04d2bd6e1400f1f24
MohammadArik <mohammadarik63@gmail.com>
2021-07-24 16:31:14 +0600

    266A solution added

---
+++ Difficulty 800/266A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    string combination;
+
+    cin >> n;
+    cin >> combination;
+
+    char prevColor = combination[0];
+    int needsToChange = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (combination[i] == prevColor)
+        {
+            needsToChange++;
+        }
+        prevColor = combination[i];
+    }
+
+    cout << needsToChange;
+
+    return 0;
+}
