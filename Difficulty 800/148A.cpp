29ec141a1a0d35b4cd852e5dc4cef0193ae6d016
MohammadArik <mohammadarik63@gmail.com>
2021-07-24 16:30:54 +0600

    148A added

---
+++ Difficulty 800/148A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+
+    int damaged = 0;
+
+    for (int i = 1; i <= d; i++)
+    {
+        if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
+        {
+            damaged++;
+        }
+    }
+
+    cout << damaged;
+
+    return 0;
+}
