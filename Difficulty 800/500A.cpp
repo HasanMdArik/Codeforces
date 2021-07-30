875d61c35325fdb6f456b5adacd3b96ac738612b
Mohammad Arik <mohammadarik63@gmail.com>
2021-07-31 04:44:09 +0600

    500A Added

---
+++ Difficulty 800/500A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, t;
+    int *cells = NULL;
+
+    cin >> n >> t;
+    t -= 1;
+    cells = new int[n];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int cell;
+        cin >> cell;
+        cells[i] = cell;
+    }
+
+    int myPosition = 0;
+    bool possibilityOfGoing = false;
+    while (myPosition <= t)
+    {
+        myPosition = myPosition + cells[myPosition];
+        if (myPosition == t)
+        {
+            possibilityOfGoing = true;
+            break;
+        }
+    }
+
+    if (possibilityOfGoing)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
